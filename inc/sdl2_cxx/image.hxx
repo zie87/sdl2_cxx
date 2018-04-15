5cf14b939d8cd2e5f2d40331abd124c7ad019ee8
zie87 <tobias.zindl@googlemail.com>
2018-04-15 15:27:51 +0200

    added combining header for sdl2 image

---
+++ inc/sdl2_cxx/image.hxx
@@ -0,0 +1,13 @@
+#ifndef SDL2_CXX_IMAGE_HXX
+#define SDL2_CXX_IMAGE_HXX
+
+/**
+ * \defgroup IMAGE image related structures and functions
+ * @{
+ */
+
+#include <sdl2_cxx/image/image.hxx>
+
+/**@}*/
+
+#endif // SDL2_CXX_IMAGE_HXX
